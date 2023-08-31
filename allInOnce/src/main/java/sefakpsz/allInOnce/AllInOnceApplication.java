9dac356cc98ae6c766f49bcf352d0d957e1d441b
sefa kapısız <sefa.kapisiz@coino.com>
2023-08-31 13:22:11 +0300

    first commit ✨

---
+++ allInOnce/src/main/java/sefakpsz/allInOnce/AllInOnceApplication.java
@@ -0,0 +1,13 @@
+package sefakpsz.allInOnce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInOnceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllInOnceApplication.class, args);
+	}
+
+}
