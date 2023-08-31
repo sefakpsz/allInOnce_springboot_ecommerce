ec181bed20cc5e8c57aaf614f7d0eb57e71112fc
sefa kapısız <sefa.kapisiz@coino.com>
2023-08-31 17:52:45 +0300

    feat: order entity intialized

--- src/main/java/sefakpsz/allInOnce/entities/Order.java
+++ src/main/java/sefakpsz/allInOnce/entities/Order.java
@@ -1,4 +1,27 @@
 package sefakpsz.allInOnce.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sefakpsz.allInOnce.enums.User.Role;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table()
 public class Order {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
